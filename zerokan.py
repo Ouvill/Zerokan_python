530188089e1ff018d07c206b6cb9c4ef5e6b606a
Ouvill <https://twitter.com/Ouvill>
2015-05-12 01:56:19 +0900

    クラス化に取りあえず成功。もう少しコミットの粒度を小さくした方が良いかも。

--- zerokan.py
+++ zerokan.py
@@ -27,13 +27,6 @@ class Setting:
 
 class Player:
     
-    killNumber=0
-    lossNumber=0
-    crashNumber=0
-    destroyNumber=0
-    destroyedNumber=0
-    wreckNumber=0
-       
     def __init__(self,name):
         self.initPlayerRecord()
         
@@ -45,12 +38,14 @@ class Player:
         self.destroyedPattern = "destroyed .*" + self.playerName
         self.wreckedPattern = self.playerName + ".* has been wrecked"
 
-        self.reKillPattern = re.compile(killPattern)
-        self.reLossPattern = re.compile(lossPattern)
-        self.reCrashPattern= re.compile(crashPattern)
-        self.reDestroyPattern= re.compile(destroyPattern)
-        self.reDestroyedPattern = re.compile(destroyedPattern)
-        self.reWreckedPattern = re.compile(wreckedPattern)
+        self.reKillPattern = re.compile(self.killPattern)
+        self.reLossPattern = re.compile(self.lossPattern)
+        self.reCrashPattern= re.compile(self.crashPattern)
+        self.reDestroyPattern= re.compile(self.destroyPattern)
+        self.reDestroyedPattern = re.compile(self.destroyedPattern)
+        self.reWreckedPattern = re.compile(self.wreckedPattern)
+
+        print(self.playerName)
 
     # player のデータを初期化
     def initPlayerRecord(self):
@@ -59,45 +54,45 @@ class Player:
         self.crashNumber=0
         self.destroyNumber=0
         self.destroyedNumber=0
-        self.wreckNumber=0
+        self.wreckedNumber=0
 
     # WT の hudmsg/damageから プレイヤー名を探して、戦果を記録
     def countResult(self,damages):
         for damage in damages:
             print(damage["id"])
-            if reKillPattern.search(damage["msg"]):
+            if self.reKillPattern.search(damage["msg"]):
                 print("kill count")
-                killNumber += 1
-            if reLossPattern.search(damage["msg"]):
+                self.killNumber += 1
+            if self.reLossPattern.search(damage["msg"]):
                 print("loss count")
-                lossNumber += 1
-            if reCrashPattern.search(damage["msg"]):
+                self.lossNumber += 1
+            if self.reCrashPattern.search(damage["msg"]):
                 print("crash count")
-                crashNumber += 1
-            if reDestroyPattern.search(damage["msg"]):
+                self.crashNumber += 1
+            if self.reDestroyPattern.search(damage["msg"]):
                 print("destroy count")
-                destroyNumber += 1
-            if reDestroyedPattern.search(damage["msg"]):
+                self.destroyNumber += 1
+            if self.reDestroyedPattern.search(damage["msg"]):
                 print("destroyed count")
-                destroyedNumber += 1
-            if reWreckedPattern.search(damage["msg"]):
+                self.destroyedNumber += 1
+            if self.reWreckedPattern.search(damage["msg"]):
                 print("wrecked count")
-                wreckedNumber += 1
+                self.wreckedNumber += 1
 
     # playerのデータを表示
     def printRecord(self):
-        print("Player's kill count",killNumber)
-        print("Player's killed count",lossNumber)
-        print("Player's crash count",crashNumber)
-        print("Player's destroy count",destroyNumber)
-        print("Player's destoryed count",destroyedNumber)
-        print("Player's wrecked count",wreckedNumber)
+        print("Player's kill count",self.killNumber)
+        print("Player's killed count",self.lossNumber)
+        print("Player's crash count",self.crashNumber)
+        print("Player's destroy count",self.destroyNumber)
+        print("Player's destoryed count",self.destroyedNumber)
+        print("Player's wrecked count",self.wreckedNumber)
 
     # player のデータを書き込み
     def writeRecord(self,startTime,endTime):
         strStartTime = startTime.strftime('%Y/%m/%d-%H:%M:%S')
         strEndTime = endTime.strftime('%Y/%m/%d-%H:%M:%S')
-        listResult = [strStartTime,strEndTime,killNumber,lossNumber,crashNumber,destroyNumber,destroyedNumber,wreckedNumber]
+        listResult = [strStartTime,strEndTime,self.killNumber,self.lossNumber,self.crashNumber,self.destroyNumber,self.destroyedNumber,self.wreckedNumber]
                 
         try:
             f = open("data.csv","a")
@@ -112,15 +107,15 @@ class Player:
     
 
 class GameInfo:
-    lastEvtMsgId = 0
-    lastDmgMsgId = 0
-    mapObj=None
-    OldMapObj=None
-
     def __init__(self):
         self.firstStep=True
-        startTime = datetime.datetime.today()
-
+        self.lastEvtMsgId = 0
+        self.lastDmgMsgId = 0
+        self.mapObj=None
+        self.OldMapObj=None
+        self.state=None
+        self.hudmsg=None
+        self.damages=None
     
     # WTの起動状態確認-aces.exe が起動していれば 0、launcher が起動していれば 1、 両方起動していなければ 2 を返す
     def getWtProcess(self):
@@ -142,29 +137,32 @@ class GameInfo:
 
     # ゲームの状況を判定する。試合をしていない状態は0、試合が開始した時は1、試合中なら2、試合が終了した時は3、を返す
     def getGameState(self):
-        if oldMapObj == "" and mapObj == "":
+        oldMapObjTxt = self.oldMapObj.text
+        mapObjTxt = self.mapObj.text
+        
+        if oldMapObjTxt == "" and  mapObjTxt== "":
             return 0
-        elif oldMapObj == ""  and mapObj != "":
+        elif oldMapObjTxt == ""  and mapObjTxt != "":
             return 1
-        elif oldMapObj != "" and mapObj != "":
+        elif oldMapObjTxt != "" and mapObjTxt != "":
             return 2
-        elif oldMapObj != "" and mapObj == "":
+        elif oldMapObjTxt != "" and mapObjTxt == "":
             return 3
 
     # aces が起動した時に行う動作。
     def acesInit(self):
-        mapObj = requests.get('http://localhost:8111/map_obj.json')
-        oldMapObj = mapObj
+        self.mapObj = requests.get('http://localhost:8111/map_obj.json')
+        self.oldMapObj = self.mapObj
         self.firstStep = False
         print("firstStep",self.firstStep)
 
     # aces 起動中に繰り返し行う動作
     def reloadLocalHost(self):
-        state = requests.get('http://localhost:8111/state').json()
-        hudmsgParams={'lastEvt':lastEvtMsgId, 'lastDmg':lastDmgMsgId}
-        hudmsg = requests.get('http://localhost:8111/hudmsg', params=hudmsgParams).json()
-        mapObj = requests.get('http://localhost:8111/map_obj.json')
-        damages = hudmsg["damage"]
+        self.state = requests.get('http://localhost:8111/state').json()
+        self.hudmsgParams={'lastEvt':self.lastEvtMsgId, 'lastDmg':self.lastDmgMsgId}
+        self.hudmsg = requests.get('http://localhost:8111/hudmsg', params=self.hudmsgParams).json()
+        self.mapObj = requests.get('http://localhost:8111/map_obj.json')
+        self.damages = self.hudmsg["damage"]
 
 setting=Setting()
 gameInfo=GameInfo()
@@ -179,6 +177,7 @@ while WtProcess < 2:
 
         if gameInfo.firstStep:
             print("firstStep",gameInfo.firstStep)
+            time.sleep(15)
             gameInfo.acesInit()
 
         # aces.exeが起動中繰り返し行う動作
@@ -189,7 +188,7 @@ while WtProcess < 2:
             except:
                 print("network error")
 
-            gameState = gameInfo.getGameState(oldMapObj.text,mapObj.text)
+            gameState = gameInfo.getGameState()
             # 試合中じゃない時
             if gameState == 0:
                 print("not gaming")
